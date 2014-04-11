9f2216d3422eed6b84ea79d62ccdd7e0cea4c10a
Toby Retallick <toby.retallick@gmail.com>
2014-04-11 10:09:29 +0100

    Answered q.2

--- scratchpad.rb
+++ scratchpad.rb
@@ -1,5 +1,12 @@
-def select_elements_starting_with_a(array)
-  array.select { |word| word if word[0,1] == 'a' }
+#def select_elements_starting_with_a(array)
+#  array.select { |word| word if word[0,1] == 'a' }
+#end
+#
+#p select_elements_starting_with_a(["apple", "orange"])
+
+def select_elements_starting_with_vowel(array)
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  array.select { |word| word if vowels.include?(word[0,1]) }
 end
 
-p select_elements_starting_with_a(["apple", "orange"])
+p select_elements_starting_with_vowel(["apple", "orange", "tank"])
