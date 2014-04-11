c5880b56274c25b9a479c9711195083146feead9
Toby Retallick <toby.retallick@gmail.com>
2014-04-11 10:58:46 +0100

    Answered q.10

--- scratchpad.rb
+++ scratchpad.rb
@@ -1,7 +1,15 @@
-# sort an array of words by their last letter, e.g.
-# ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
-def array_sort_by_last_letter_of_word(array)
-  array.sort_by { |word| word[-1] }
+# cut strings in half, and return the first half, e.g.
+# 'banana' becomes 'ban'. If the string is an odd number of letters
+# round up - so 'apple' becomes 'app'
+def get_first_half_of_string(string)
+  slice_value = ((string.length.to_f / 2).ceil - 1)
+  string.slice(0..slice_value)
 end
 
-p array_sort_by_last_letter_of_word(['sky', 'puma', 'maker'])
+p get_first_half_of_string('banana')
+p get_first_half_of_string('apple')
+p get_first_half_of_string('rivka')
+p get_first_half_of_string('toby')
+
+#p 'banana'.length / 2
+#p (('apple'.length).to_f / 2).ceil
