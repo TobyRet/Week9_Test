2e088a2411ab79c903ae5da9456a1cca4fb8a11d
Toby Retallick <toby.retallick@gmail.com>
2014-04-11 11:12:57 +0100

    Answered q.11

--- scratchpad.rb
+++ scratchpad.rb
@@ -1,15 +1,7 @@
-# cut strings in half, and return the first half, e.g.
-# 'banana' becomes 'ban'. If the string is an odd number of letters
-# round up - so 'apple' becomes 'app'
-def get_first_half_of_string(string)
-  slice_value = ((string.length.to_f / 2).ceil - 1)
-  string.slice(0..slice_value)
+def make_numbers_negative(number)
+  number > 0 ? -number : number
+
 end
 
-p get_first_half_of_string('banana')
-p get_first_half_of_string('apple')
-p get_first_half_of_string('rivka')
-p get_first_half_of_string('toby')
+p make_numbers_negative(-5)
 
-#p 'banana'.length / 2
-#p (('apple'.length).to_f / 2).ceil
