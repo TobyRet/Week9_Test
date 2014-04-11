8af74c74062cc0198423b5dea30ebf835613de51
Toby Retallick <toby.retallick@gmail.com>
2014-04-11 10:43:23 +0100

    Answered q.8

--- scratchpad.rb
+++ scratchpad.rb
@@ -1,7 +1,6 @@
-# discard the first 3 elements of an array, 
-# e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
-def all_elements_except_first_3(array)
-  array[3..-1]
+# add an element to the beginning of an array
+def add_element_to_beginning_of_array(array, element)
+  array.unshift(element)
 end
 
-p all_elements_except_first_3([1, 2, 3, 4, 5, 6] )
+p add_element_to_beginning_of_array([0,0,0], 1)
