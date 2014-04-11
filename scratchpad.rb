b817e848a04842bd9bcc3fdf8d95b95e2e1446f7
Toby Retallick <toby.retallick@gmail.com>
2014-04-11 11:26:43 +0100

    Answered q.15

--- scratchpad.rb
+++ scratchpad.rb
@@ -1,6 +1,13 @@
 # return the shortest word in an array
-def shortest_word_in_array(array)
-  array.sort_by { |word| word.length }.shift
+#def shortest_word_in_array(array)
+#  array.sort_by { |word| word.length }.shift
+#end
+#
+#p shortest_word_in_array(['handcream', 'coffee'])
+
+# return the longest word in an array
+def longest_word_in_array(array)
+  array.sort_by { |word| word.length }.reverse.shift
 end
 
-p shortest_word_in_array(['handcream', 'coffee'])
+p longest_word_in_array(['handcream', 'coffee'])
